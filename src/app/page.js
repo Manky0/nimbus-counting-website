3ea4471e94653bd1e32d8c09db969ac9f292298d
Valdinei Rodrigues <valvsari@gmail.com>
2023-11-06 21:56:28 -0300

    aaaaaaaaaaaaaa

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,22 @@ import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 import AboutCompany from "./components/CompanySection";
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-R3J9004T8N" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-R3J9004T8N');
+        `}
+      </Script>
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AchievementsSection />
