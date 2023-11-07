ea103ca5ceab42006d01fb76da9e1770043a8aaa
Valdinei Rodrigues <valvsari@gmail.com>
2023-11-06 22:06:11 -0300

    vercel analytics 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from '@vercel/analytics/react';
+
 console.log('test')
 const App = ({ Component, pageProps }) => {
   return (
     <>
       <GoogleAnalytics trackPageViews gaMeasurementId='G-R3J9004T8N' />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 };
